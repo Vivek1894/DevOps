b5a994ee7973833dc092d2b9ce5018997213411c
Vivek <vivekchaudhary161894@gmail.com>
2023-11-20 18:01:56 +0530

    Scripts added

---
+++ bash_scripts/basic_calc.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# This first line of the script is shebang which tell the system how to execute
+echo "-------------------------------"
+echo "Bash script is fun with @tws"
+echo "-------------------------------"
+sleep 0.5
+echo 
+
+# Reading user inpunt with variable
+echo "-------------------------"
+read -p "Enter your firstname: " firstname
+echo "-------------------------"
+sleep 0.5
+echo "--------------------"
+read -p "Enetr your sirname: " sirname
+echo "--------------------"
+sleep 0.5
+echo "--------------------------------------------------"
+echo "Welcome ***$firstname $sirname***"
+echo "--------------------------------------------------"
+echo
+sleep 0.5
+
+
+# Taking input from user for operation 
+while true; do
+	
+	read -p "Enter the first number: " num1
+	read -p "Enter the second number: " num2
+	read -p "Enter operator to perform action (+,-,/,*): " operator
+
+		if [ "$operator" = "+" ]; then
+			result=$((num1+num2))
+
+		elif [ "$operator" = "-" ]; then
+			result=$((num1-num2))
+
+		elif [ "$operator" = "*" ]; then
+        		result=$((num1*num2))
+
+		elif [ "$operator" = "/" ]; then
+        		result=$((num1/num2))	
+		else 
+	echo " Invalid operator Chosen "
+	exit 1
+	fi
+
+	echo "The result  for $num1 $operator $num2 is : $result"
+
+	# Asking user if the want to continue or exit
+	read -p "Do you want to continue (Y/N)? " choice 
+
+	if [ "$choice" = "Y" ] || [ "$choice" = "y" ]; then
+		continue
+	else 
+		echo -n "Exiting"
+		for i in {1..3}; do
+			echo -n "."
+			sleep 0.5 
+		done
+		echo ""
+		echo "Thank You $firstname $sirname, See You Soon!!!!!!!!"
+		break
+	fi
+done
+
+
+
+
+
+
+
---
+++ bash_scripts/script1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 0.5 
+echo "########################################"
+echo "I Will Complete #90daysofdevops Challenge"
+echo "#########################################"
